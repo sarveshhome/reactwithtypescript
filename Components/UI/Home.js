260d59162bd9827099c04a594decd13299f7f9df
sarveshhome <sarvesh.carlton@gmail.com>
2021-06-30 14:26:15 +0100

    4 files modified

--- Components/UI/Home.js
+++ Components/UI/Home.js
@@ -1,7 +1,14 @@
+import React from 'react';
+
+import Hello from '../Hello.js';
+
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      {/* <Hello name={this.state.name} /> */}
+      {/* <Person name={this.state.PersonName} />
+      <Book name={this.state.BookName} /> */}
     </div>
   );
 };
