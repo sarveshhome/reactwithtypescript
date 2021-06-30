d440b300afb435879d93056841309c9410f201a4
sarveshhome <sarvesh.carlton@gmail.com>
2021-06-30 19:28:25 +0100

    2 files modified, 2 added

--- Components/UI/Address.js
+++ Components/UI/Address.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import styles from './Todo.module.css';
+import AddressEnter from './AddressEnter.js';
+import {
+  Container,
+  Form,
+  Button,
+  Row,
+  ListGroup,
+  Col,
+  FormGroup
+} from 'react-bootstrap';
+
 const Address = () => {
+  const [contaddres, setCountaddress] = useState();
+
+  const addressHandler = event => {
+    return <AddressEnter />;
+  };
+
   return (
     <div>
-      <h1> Address</h1>
+      {addressHandler()}
+      <Button variant="primary" onClick={addressHandler} type="submit">
+        + New Address
+      </Button>
     </div>
   );
 };
