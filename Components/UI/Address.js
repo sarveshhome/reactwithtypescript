89ac217111670c69e5c560d693d4d40b5c0162ee
sarveshhome <sarvesh.carlton@gmail.com>
2021-06-30 20:35:28 +0100

    1 file modified

--- Components/UI/Address.js
+++ Components/UI/Address.js
@@ -18,7 +18,7 @@ const Address = () => {
   const [values, setValues] = useState('');
   const [count, setCount] = useState(0);
   useEffect(() => {
-    setValues('2');
+    setValues('<div><h1>hello</h1></div>');
   }, []);
   const addressHandler = event => {
     event.preventDefault();
@@ -27,11 +27,19 @@ const Address = () => {
     setComponentcall(final);
     setCount(count + 1);
   };
-
+  const htmlDecode = input => {
+    var e = document.createElement('div');
+    e.innerHTML = input;
+    return e.childNodes.length === 0 ? '' : e.childNodes[0].nodeValue;
+  };
   return (
     <div>
       {componentcall &&
-        componentcall.map(data => <span key={data.id}> {data.todovalue}</span>)}
+        componentcall.map(data => (
+          <span key={data.id}>
+            dangerouslySetInnerHTML={{ __html: htmlDecode(data.data) }}
+          </span>
+        ))}
       <Button variant="primary" onClick={addressHandler} type="submit">
         + New Address
       </Button>
